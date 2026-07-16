bdaed860b165a4325b23fc6cc54ddb790d4f5954
MTSAMAL <mariam.tsamalashvili@exxeta.com>
2026-07-16 13:45:18 +0400

    test

--- main.tf
+++ main.tf
@@ -16,3 +16,18 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-12334999"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
